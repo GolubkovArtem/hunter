6dd6c0bfb5892d721062b538121a8028f71ce78a
GolubkovArtem <artemgolubkov@bk.ru>
2017-11-03 15:24:12 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/GolubkovArtem/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    e803ab17e04867e082d1afe634666a6370a077de
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
